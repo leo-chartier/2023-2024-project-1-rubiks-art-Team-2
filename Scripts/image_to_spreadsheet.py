8ea67e51446fa1d0367b47eda994f741b0517bc8
Léo CHARTIER <leo.chartier@algosup.com>
2023-10-26 09:15:03 +0200

    Add config file and coordinates on fresco

--- Scripts/image_to_spreadsheet.py
+++ Scripts/image_to_spreadsheet.py
@@ -1,4 +1,32 @@
-# If modifying the scopes, delete the file token.json.
+# If modifying the scopes or if the token is expired, delete the file token.json.
+
+# Note: Some modifications have been made to adapt to the specifications of team 3.
+
+# Notation for the moves configuration:
+"""
+   123
+   4U6
+   789
+123123123123
+4L64F64R64B6
+789789789789
+   123
+   4D6
+   789
+(middle of a face is 5)
+
+Uppercase letter: Single layer
+Lowercase letter: Double layer (Face layer + middle layer)
+Only a letter: 90° clockwise
+Letter with apostrophe: 90° counterclockwise
+Letter with 2: 180° rotation
+
+Example:
+A 90° counterclockwise rotation of the left face would be written as L'
+One of the moves from this rotation is the facelet from U1 moving to B9
+"""
+
+
 
 import json
 import os
@@ -23,7 +51,71 @@ except ImportError:
 
 
 
+DEFAULT_CONFIG = {
+    "image_path": "", # Path to the image you want to convert
+    "spreadsheet_id": "", # ID of the spreadsheet to use
+    "scopes": ["https://www.googleapis.com/auth/spreadsheets"],
+    "fresco_id": -1, # ID of the sheet with the fresco preview
+    "moves_id": -1, # ID of the sheet with the list of moves for each cube
+    "cell_size": 5, # Width and height of a cell on the preview
+    "sections": {
+        # Dividing the work in smaller parts, number are in cubes
+        "width": 5,
+        "height": 5,
+        "offsetX": 0,
+        "offsetY": 0
+    },
+    "default_front": "green", # Defined by https://bestsiteever.ru/algs_for_mosaic/
+    "default_up": "white", # Defined by https://bestsiteever.ru/algs_for_mosaic/
+    "converter_colors": [
+        # Defined by https://bestsiteever.ru/algs_for_mosaic/
+        [255, 255, 255],
+        [255, 255,   0],
+        [255,   0,   0],
+        [  0, 255,   0],
+        [  0,   0, 255],
+        [255, 153,   0]
+    ],
+    "colors": {
+        "white":  {"red": 1, "green": 1,    "blue": 1},
+        "yellow": {"red": 1, "green": 1,    "blue": 0},
+        "blue":   {"red": 0, "green": 0.5,  "blue": 1},
+        "green":  {"red": 0, "green": 0.75, "blue": 0},
+        "red":    {"red": 1, "green": 0,    "blue": 0},
+        "orange": {"red": 1, "green": 0.5,  "blue": 0}
+    },
+    "moves": {
+        "F": {"F1": "F3", "F2": "F6", "F3": "F9", "F4": "F2", "F6": "F8", "F7": "F1", "F8": "F4", "F9": "F7", "U7": "R1", "U8": "R4", "U9": "R7", "R1": "D1", "R4": "D2", "R7": "D3", "D1": "L3", "D2": "L6", "D3": "L9", "L3": "U7", "L6": "U8", "L9": "U9"},
+        "B": {"B1": "B3", "B2": "B6", "B3": "B9", "B4": "B2", "B6": "B8", "B7": "B1", "B8": "B4", "B9": "B7", "U3": "L1", "U2": "L4", "U1": "L7", "L1": "D9", "L4": "D8", "L7": "D7", "D9": "R9", "D8": "R6", "D7": "R3", "R9": "U3", "R6": "U2", "R3": "U1"},
+        "U": {"U1": "U3", "U2": "U6", "U3": "U9", "U4": "U2", "U6": "U8", "U7": "U1", "U8": "U4", "U9": "U7", "B3": "R3", "B2": "R2", "B1": "R1", "R3": "F3", "R2": "F2", "R1": "F1", "F3": "L3", "F2": "L2", "F1": "L1", "L3": "B3", "L2": "B2", "L1": "B1"},
+        "D": {"D1": "D3", "D2": "D6", "D3": "D9", "D4": "D2", "D6": "D8", "D7": "D1", "D8": "D4", "D9": "D7", "F7": "R7", "F8": "R8", "F9": "R9", "R7": "B7", "R8": "B8", "R9": "B9", "B7": "L7", "B8": "L8", "B9": "L9", "L7": "F7", "L8": "F8", "L9": "F9"},
+        "L": {"L1": "L3", "L2": "L6", "L3": "L9", "L4": "L2", "L6": "L8", "L7": "L1", "L8": "L4", "L9": "L7", "U1": "F1", "U4": "F4", "U7": "F7", "F1": "D1", "F4": "D4", "F7": "D7", "D1": "B9", "D4": "B6", "D7": "B3", "B9": "U1", "B6": "U4", "B3": "U7"},
+        "R": {"R1": "R3", "R2": "R6", "R3": "R9", "R4": "R2", "R6": "R8", "R7": "R1", "R8": "R4", "R9": "R7", "U9": "B1", "U6": "B4", "U3": "B7", "B1": "D9", "B4": "D6", "B7": "D3", "D9": "F9", "D6": "F6", "D3": "F3", "F9": "U9", "F6": "U6", "F3": "U3"},
+        "M": {"U2": "F2", "U5": "F5", "U8": "F8", "F2": "D2", "F5": "D5", "F8": "D8", "D2": "B8", "D5": "B5", "D8": "B2", "B8": "U2", "B5": "U5", "B2": "U8"},
+        "E": {"F4": "R4", "F5": "R5", "F6": "R6", "R4": "B4", "R5": "B5", "R6": "B6", "B4": "L4", "B5": "L5", "B6": "L6", "L4": "F4", "L5": "F5", "L6": "F6"},
+        "S": {"U4": "R2", "U5": "R5", "U6": "R8", "R2": "D6", "R5": "D5", "R8": "D4", "D6": "L8", "D5": "L5", "D4": "L2", "L8": "U4", "L5": "U5", "L2": "U6"},
+        "x": {"L3": "L1", "L6": "L2", "L9": "L3", "L2": "L4", "L8": "L6", "L1": "L7", "L4": "L8", "L7": "L9", "F1": "U1", "F4": "U4", "F7": "U7", "D1": "F1", "D4": "F4", "D7": "F7", "B9": "D1", "B6": "D4", "B3": "D7", "U1": "B9", "U4": "B6", "U7": "B3", "F2": "U2", "F5": "U5", "F8": "U8", "D2": "F2", "D5": "F5", "D8": "F8", "B8": "D2", "B5": "D5", "B2": "D8", "U2": "B8", "U5": "B5", "U8": "B2", "R1": "R3", "R2": "R6", "R3": "R9", "R4": "R2", "R6": "R8", "R7": "R1", "R8": "R4", "R9": "R7", "U9": "B1", "U6": "B4", "U3": "B7", "B1": "D9", "B4": "D6", "B7": "D3", "D9": "F9", "D6": "F6", "D3": "F3", "F9": "U9", "F6": "U6", "F3": "U3"},
+        "y": {"U1": "U3", "U2": "U6", "U3": "U9", "U4": "U2", "U6": "U8", "U7": "U1", "U8": "U4", "U9": "U7", "B3": "R3", "B2": "R2", "B1": "R1", "R3": "F3", "R2": "F2", "R1": "F1", "F3": "L3", "F2": "L2", "F1": "L1", "L3": "B3", "L2": "B2", "L1": "B1", "R4": "F4", "R5": "F5", "R6": "F6", "B4": "R4", "B5": "R5", "B6": "R6", "L4": "B4", "L5": "B5", "L6": "B6", "F4": "L4", "F5": "L5", "F6": "L6", "D3": "D1", "D6": "D2", "D9": "D3", "D2": "D4", "D8": "D6", "D1": "D7", "D4": "D8", "D7": "D9", "R7": "F7", "R8": "F8", "R9": "F9", "B7": "R7", "B8": "R8", "B9": "R9", "L7": "B7", "L8": "B8", "L9": "B9", "F7": "L7", "F8": "L8", "F9": "L9"},
+        "z": {"F1": "F3", "F2": "F6", "F3": "F9", "F4": "F2", "F6": "F8", "F7": "F1", "F8": "F4", "F9": "F7", "U7": "R1", "U8": "R4", "U9": "R7", "R1": "D1", "R4": "D2", "R7": "D3", "D1": "L3", "D2": "L6", "D3": "L9", "L3": "U7", "L6": "U8", "L9": "U9", "U4": "R2", "U5": "R5", "U6": "R8", "R2": "D6", "R5": "D5", "R8": "D4", "D6": "L8", "D5": "L5", "D4": "L2", "L8": "U4", "L5": "U5", "L2": "U6", "B3": "B1", "B6": "B2", "B9": "B3", "B2": "B4", "B8": "B6", "B1": "B7", "B4": "B8", "B7": "B9", "L1": "U3", "L4": "U2", "L7": "U1", "D9": "L1", "D8": "L4", "D7": "L7", "R9": "D9", "R6": "D8", "R3": "D7", "U3": "R9", "U2": "R6", "U1": "R3"}
+    }
+}
+
 config = {}
+
+def load_config():
+    global config
+    os.chdir(os.path.dirname(__file__))
+    if not os.path.isfile("config.json"):
+        with open("config.json", "w") as f:
+            json.dump(DEFAULT_CONFIG, f)
+        raise FileNotFoundError("An empty configuration file has been created. Please fill it out before continuing.")
+    with open("config.json", "r") as f:
+        config = json.load(f)
+    return config
+
+
+
+
 
 class Face:
     def __init__(self, colors: list[str], moves: str) -> None:
@@ -35,13 +127,15 @@ class Face:
         return "|".join(self.colors) + " " + self.moves
 
 def format_coord(x: int, y: int) -> str:
-    number = x + 1
+    # number = x + 1
+    number = y + 1 # TEMP: Team 3
     text = ""
     while number > 0:
         remainder = (number - 1) % 26
         text = chr(65 + remainder) + text
         number = (number - 1) // 26
-    return f"{text}{y + 1}"
+    # return f"{text}{y + 1}"
+    return f"{text}{x + 1}"
 
 # Adapted from https://gist.github.com/florentbr/349b1ab024ca9f3de56e6bf8af2ac69e
 JS_DROP_FILES = """
@@ -144,13 +238,9 @@ def write_to_spreadsheets(moves: list[list[Face]]):
         with open('token.json', 'w') as token:
             token.write(creds.to_json())
 
+    # TODO: Make requests a bit better looking than just a JSON file (or load default from file and modify)
     requests = [
         ## Fresco
-        # Clear the formatting (colors)
-        {"updateCells": {
-            "range": {"sheetId": config["fresco_id"]},
-            "fields": "*"
-        }},
         # Clear the borders
         {"updateBorders": {
             "range": {
@@ -222,8 +312,11 @@ def write_to_spreadsheets(moves: list[list[Face]]):
                     "startColumnIndex": j,
                     "endColumnIndex": j + 1
                 },
-                "rows": [{"values": [{"userEnteredFormat": {"backgroundColor": config["colors"][color]}}]}],
-                "fields": "userEnteredFormat.backgroundColor"
+                "rows": [{"values": [{"userEnteredFormat": {
+                    "backgroundColor": config["colors"][color],
+                    "textFormat": {"foregroundColor": config["colors"][color]}
+                }}]}],
+                "fields": "*"
             }})
     
     # Set the border for every cube and section
@@ -258,10 +351,21 @@ def write_to_spreadsheets(moves: list[list[Face]]):
                 "left": {"style": "SOLID_THICK", "width": 1, "color": {}},
                 "right": {"style": "SOLID_THICK", "width": 1, "color": {}},
             }})
+    
+    # Set the coordinates on the fresco
+    fresco_rows_text = []
+    for i in range(len(moves)):
+        line = []
+        for j in range(len(moves[i])):
+            for _ in range(3):
+                # line.append(format_coord(j, i))
+                line.append(f"{format_coord(j, i)} - {j // 12 + 1}.{11 - i // 3}.{(j % 12) // 4 + 1}") # TEMP: Team 3
+        for _ in range(3):
+            fresco_rows_text.append(line)
 
     # Set the instructions
     i = 0
-    rows_text = [("Coords", "Status", "Front", "Up", "Moves")]
+    moves_rows_text = [("Coords", "Status", "Front", "Up", "Moves")]
     for y, row in enumerate(reversed(moves)):
         for x, move in enumerate(row):
             i += 1
@@ -270,7 +374,7 @@ def write_to_spreadsheets(moves: list[list[Face]]):
             # TODO: Correct orientation
             front_rgb = config["colors"][front]
             up_rgb = config["colors"][up]
-            rows_text.append((format_coord(x, y), "0", front, up, move.moves))
+            moves_rows_text.append((format_coord(x, len(moves) - 1 - y), "0", front, up, move.moves))
             requests.append({
                 "updateCells": {
                     "rows": [
@@ -322,9 +426,15 @@ def write_to_spreadsheets(moves: list[list[Face]]):
         ).execute()
         sheets.values().update(
             spreadsheetId=config["spreadsheet_id"],
+            range=sheets_names_by_id[config["fresco_id"]] + "!1:" + str(len(fresco_rows_text)),
+            valueInputOption="USER_ENTERED",
+            body={"values": fresco_rows_text}
+        ).execute()
+        sheets.values().update(
+            spreadsheetId=config["spreadsheet_id"],
             range=sheets_names_by_id[config["moves_id"]] + "!A:E",
             valueInputOption="USER_ENTERED",
-            body={"values": rows_text}
+            body={"values": moves_rows_text}
         ).execute()
 
     except HttpError as err:
@@ -333,10 +443,7 @@ def write_to_spreadsheets(moves: list[list[Face]]):
 
 
 def main():
-    global config
-    os.chdir(os.path.dirname(__file__))
-    with open("config.json", "r") as f:
-        config = json.load(f)
+    load_config()
     soup = convert_image(config["image_path"])
     moves = extract_moves(soup)
     write_to_spreadsheets(moves)
